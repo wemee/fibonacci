2aeeab176f96c2ced7ebea2680ec9d31e90cf4ef
Jr-Yung Tsai <wemee7012@gmail.com>
2017-08-18 10:50:34 +0800

    重構

--- fibonacci.rb
+++ fibonacci.rb
@@ -2,22 +2,20 @@ module Fibonacci
 	private
 	def recursive_finding! n
 		return n if n<0
-		return @num_cache[n]  if @num_cache[n]
-		@num_cache[n] = recursive_finding!(n-1) + recursive_finding!(n-2)
+		@num_cache[n]  ||= recursive_finding!(n-1) + recursive_finding!(n-2)
 	end
 
 	public
 	def find_at n
-		@num_cache ||= [0, 0, 1]
+		@num_cache ||= [0, 1]
 		return n if n<0
-		return @num_cache[n]  if @num_cache[n]
-		@num_cache[n] = recursive_finding!(n-1) + recursive_finding!(n-2)
+		@num_cache[n] ||= recursive_finding!(n)
 	end
 
 	def ary_to n
-		@num_cache ||= [0, 0, 1]
+		@num_cache ||= [0, 1]
 		find_at(n) if @num_cache.length-1 < n
-		return @num_cache[1..n]
+		return @num_cache.first(n)
 	end
 
 	module_function :recursive_finding!, :find_at, :ary_to
